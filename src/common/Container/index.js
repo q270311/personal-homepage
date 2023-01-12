d46bbaaa90982d844155e2900c5ed05ad23ca207
Michał Bałdyga <q270311@gmail.com>
2023-01-12 22:31:57 +0100

    Container component added

---
+++ src/common/Container/index.js
@@ -0,0 +1,9 @@
+import {Main} from "./styled";
+
+const Container = ({ children }) => (
+    <Main>
+        {children}
+    </Main>
+);
+
+export default Container;
---
+++ src/common/Container/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    max-width: 1216px;
+    margin: 115px auto;
+`;
