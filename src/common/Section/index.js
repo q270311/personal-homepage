32b27ab38089873f9e8c40c2917faddb1eabb546
Michał Bałdyga <q270311@gmail.com>
2023-01-12 22:32:33 +0100

    Section component added

---
+++ src/common/Section/index.js
@@ -0,0 +1,12 @@
+import { StyledSection, Title } from "./styled";
+
+const Section = ({ title, body }) => (
+    <StyledSection>
+        <Title>
+            {title}
+        </Title>
+        {body}
+    </StyledSection>
+);
+
+export default Section;
