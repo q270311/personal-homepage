b398b5211bafc3059e918c5f288cdb8b810bb073
Michał Bałdyga <q270311@gmail.com>
2023-01-17 08:47:31 +0100

    download projects name from Github function added

---
+++ src/getProjectsFromGithub.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getProjectsFromGithub = async () => {
+    const response = await axios.get("/personal-homepage/exampleProjects.json");
+    //const response = await axios.get("https://api.github.com/users/q270311/repos");
+    
+    if (!response.ok) {
+        new Error(response.statusText);
+    }
+    return await response.data;
+};
+
+
