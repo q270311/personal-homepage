dc23f9aeba7697b4a65a3d77c4a70b02305312e4
Michał Bałdyga <q270311@gmail.com>
2023-01-17 21:37:49 +0100

    Portfolio component respons to status of loading

--- src/features/Portfolio/index.js
+++ src/features/Portfolio/index.js
@@ -1,17 +1,39 @@
 import { useSelector } from "react-redux";
-import { selectProjects } from '../portfolioSlice';
+import { selectProjects, selectStatus, selectError } from '../portfolioSlice';
+import { Spinner } from './styled';
 
 const Portfolio = () => {
     const projects = useSelector(selectProjects);
-    
+    const status = useSelector(selectStatus);
+    const error = useSelector(selectError);
+
+    if (status) {
+        return (
+            <>
+                <p>Please wait, projects are being loaded...</p>
+                <Spinner />
+            </>
+        );
+    }
+    if (error) {
+        return (
+            <>
+                <p>Ooops! Somethink went wrong...</p>
+                <p>Sorry, failed to load Github projects.</p><p>You can check them directly on Github.</p>
+                <button>Go to Github</button>
+            </>
+        );
+    }
     return (
-        <ul>
-            {projects.map(project => (
-                <li key={project.id}>
-                    {project.name}
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul>
+                {projects.map(project => (
+                    <li key={project.id}>
+                        {project.name}
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 };
 
