2f55335dbe79548796ec676388ad5ad3dc46e49e
Michał Bałdyga <q270311@gmail.com>
2023-01-17 21:34:31 +0100

    renamed symbols in portfolio saga

--- src/features/portfolioSaga.js
+++ src/features/portfolioSaga.js
@@ -1,15 +1,14 @@
 import { call, put, delay } from "redux-saga/effects";
-import { setProjects } from "./portfolioSlice";
+import { fetchProjectsSuccess, fetchProjectsError } from "./portfolioSlice";
 import { getProjectsFromGithub } from ".././getProjectsFromGithub";
 
 function* fetchProjectsFromGithubHandler() {
     try {
         yield delay(2000);
-        const projects = yield call(getProjectsFromGithub); 
-        console.log(projects) ;      
-        yield put(setProjects(projects));
+        const projects = yield call(getProjectsFromGithub);
+        yield put(fetchProjectsSuccess(projects));        
     } catch (error) {
-        yield call(alert, "Coś poszło nie tak");
+        yield put(fetchProjectsError());
     }
 }
 
