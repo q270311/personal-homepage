abeffd6bb53d14462a9e832ce7cf6bb2e4f49ded
Michał Bałdyga <q270311@gmail.com>
2023-01-17 21:35:21 +0100

    add loading and error status to portfolio stor

--- src/features/portfolioSlice.js
+++ src/features/portfolioSlice.js
@@ -4,16 +4,28 @@ const portfolioSlice = createSlice({
     name: "portfolio",
     initialState: {
         projects: [],
+        loading: true,
+        error: false,
     },
     reducers: {
-        setProjects: (state, { payload }) => {
+        fetchProjectsSuccess: (state, { payload }) => {
             state.projects = payload;
+            state.loading = false;
         },
-    },    
+        fetchProjectsError: (state) => {
+            state.loading = false;
+            state.error = true;
+        },
+    },
 });
+export const {
+    fetchProjectsSuccess,
+    fetchProjectsError,
+} = portfolioSlice.actions;
 
 export const selectPortfolioState = (state) => state.portfolio;
 export const selectProjects = (state) => selectPortfolioState(state).projects;
+export const selectStatus = (state) => selectPortfolioState(state).loading;
+export const selectError = (state) => selectPortfolioState(state).error;
 
-export const { setProjects } = portfolioSlice.actions;
 export default portfolioSlice.reducer;
