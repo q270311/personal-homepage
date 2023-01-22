e0b26bc3a024b7dc8478571c7db7c06b0dffe10b
Michał Bałdyga <q270311@gmail.com>
2023-01-22 22:14:02 +0100

    display grid added in Main component

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    grid-gap: 73px;
+    
     max-width: 1216px;
     margin: 0px auto;
     padding-top:115px;
