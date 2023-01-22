59bc9579906f7c9c8ab801db88b645c232bab941
Michał Bałdyga <q270311@gmail.com>
2023-01-22 22:12:14 +0100

    textSecondary color addet to theme

--- src/theme.js
+++ src/theme.js
@@ -9,7 +9,7 @@ export const theme = {
         slateGray: "#6E7E91",
         mineShaft: "rgba(54, 54, 54, 0.72)",
         darkMode: "rgba(37, 37, 37, 1)",
-
+        textSecondary: "rgba(110, 126, 145, 1)",
     },
     breakpoint: {
         mobileMax: 767,
