80c95c14923a8606f64ee78c2f3dde18adaffa35
Michał Bałdyga <q270311@gmail.com>
2023-01-26 21:54:57 +0100

    changed colors in StyledTile component

--- src/features/Portfolio/Tile/styled.js
+++ src/features/Portfolio/Tile/styled.js
@@ -6,10 +6,8 @@ export const StyledTile = styled.div`
     padding: 56px;
     
     background: ${({ theme }) => theme.color.white};
-    //border: 6px solid ${({ theme }) => theme.color.iron};
-    //box-shadow: 0px -2px 50px ${({ theme }) => theme.color.violet02}, ${({ theme }) => theme.color.violet03};
-    border: 6px solid rgba(209, 213, 218, 0.3);
-    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+    border: 6px solid ${({ theme }) => theme.color.iron};
+    box-shadow: 0px -2px 50px ${({ theme }) => theme.color.violet02}, ${({ theme }) => theme.color.violet03};
     border-radius: 4px;
 `;
 export const StyledH1 = styled.h1`
