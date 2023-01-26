4082245815384cdcd677ff54350edfc006a442a1
Michał Bałdyga <q270311@gmail.com>
2023-01-26 21:55:06 +0100

    throw err to test getProjectsFromGithub fucntion

--- src/getProjectsFromGithub.js
+++ src/getProjectsFromGithub.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const getProjectsFromGithub = async () => {
+    throw new Error("test");
     const response = await axios.get("/personal-homepage/exampleProjects.json");
     //const response = await axios.get("https://api.github.com/users/q270311/repos");
     
