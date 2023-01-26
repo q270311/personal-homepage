74acd410e53040e3c15af221e153359a2cd43ded
Michał Bałdyga <q270311@gmail.com>
2023-01-26 21:44:43 +0100

    change color in theme

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ export const theme = {
         blackText: "#252525",
         lightGrey: "#E5E5E5",
         mercury: "#E5E5E5",
-        iron: "#D1D5DA",
+        iron: "rgba(209, 213, 218, 0.3)",
         slateGray: "#6E7E91",
         mineShaft: "rgba(54, 54, 54, 0.72)",
         darkMode: "rgba(37, 37, 37, 1)",
