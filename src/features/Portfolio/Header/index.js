7736305cb0d10c52ac6200f32fa91ea06712e207
Michał Bałdyga <q270311@gmail.com>
2023-01-26 21:53:42 +0100

    PortfolioHeader component created

---
+++ src/features/Portfolio/Header/index.js
@@ -0,0 +1,16 @@
+import SVGLogoGithub from './SVGLogoGithub';
+import { Wrapper, Title, SubTitle } from './styled';
+
+const PortfolioHeader = ({children}) => {
+
+    return (
+        <Wrapper>
+            <div><SVGLogoGithub /></div>
+            <Title>Portfolio</Title>
+            <SubTitle>My recent projects</SubTitle>   
+            {children}         
+        </Wrapper>
+    );
+};
+
+export default PortfolioHeader;
---
+++ src/features/Portfolio/Header/styled.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center; 
+    text-align:center;
+`;
+export const Title = styled.p`
+    font-style: normal;
+    font-weight: 900;
+    font-size: 30px;
+    line-height: 36px;
+    letter-spacing: 0.05em;
+    color: ${({ theme }) => theme.color.blackText};
+    margin-top: 13px;
+    margin-bottom: 10px;
+`;
+export const SubTitle = styled.p`
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;    
+    letter-spacing: 0.05em;
+    color: ${({ theme }) => theme.color.blackText}; 
+`;
+
+export const Button = styled.button`
+    padding: 12px 16px;
+    width: 168px;
+    height: 49px;    
+    border: 1px solid ${({ theme }) => theme.color.mainBlue};
+    border-radius: 4px;
+    background-color: ${({ theme }) => theme.color.mainBlue}; 
+
+    font-style: normal;
+    font-weight: 600;
+    letter-spacing: 0.05em;
+    color: ${({ theme }) => theme.color.white}; 
+`;
+export const Strong = styled.strong`
+    margin-top: 35px;
+    margin-bottom: 35px;
+
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
+    text-align: center;
+    letter-spacing: 0.05em;
+    color: ${({ theme }) => theme.color.blackText}; 
+`;
