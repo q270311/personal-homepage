981b8b6ac6387c1154e72512a875f087f3c23d34
Michał Bałdyga <q270311@gmail.com>
2023-02-01 08:55:45 +0100

    media query to list and listy item added

--- src/features/Set/styled.js
+++ src/features/Set/styled.js
@@ -6,13 +6,14 @@ export const Ul = styled.ul`
     margin: 32px 0px;
     padding: 0px;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {   
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        column-count: 1; 
+        margin: 0px;  
     }
 `;
 export const Li = styled.li`
     padding:0px;
-    margin:4px 0px;
-    font-style: normal;
+    margin:4px 0px;    
     font-weight: 400;
     font-size: 18px;
     line-height: 30px;
@@ -28,6 +29,8 @@ export const Li = styled.li`
         color: ${({ theme }) => theme.color.white};
     `};
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {   
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) { 
+        font-size: 14px;
+        line-height: 17px;  
     }
 `;
