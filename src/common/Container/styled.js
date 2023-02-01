783fa0a51bab4e9116ea33b6812e25f497160c2e
Michał Bałdyga <q270311@gmail.com>
2023-02-01 08:55:01 +0100

    media query to Main component added

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -4,10 +4,14 @@ export const Main = styled.main`
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: auto;
-    grid-gap: 73px;
-    
+    grid-gap: 74px;    
     max-width: 1216px;
     margin: 0px auto;
-    padding-top:115px;
-    padding-bottom: 115px;
+    padding: 115px 0px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) { 
+        padding: 0px;
+        margin: 0px;
+        grid-gap: 50px;
+    }
 `;
