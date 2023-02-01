05db39bf646383b2f2038196bbd8361df45fe020
Michał Bałdyga <q270311@gmail.com>
2023-02-01 08:48:14 +0100

    set width Body component to 100%

--- src/common/Body/styled.js
+++ src/common/Body/styled.js
@@ -1,6 +1,7 @@
 import styled, { css } from "styled-components";
 
 export const StyledBody = styled.div`
+    width: 100%;
     background-color: ${({ theme }) => theme.color.whiteMode}; 
     transition: background-color 800ms linear;
 
