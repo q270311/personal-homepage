93f24d590106d79811b1ed4c258408118dd428c1
Michał Bałdyga <q270311@gmail.com>
2023-02-03 21:58:47 +0100

    downloading projects from api not from ofline file

--- src/features/portfolioSaga.js
+++ src/features/portfolioSaga.js
@@ -1,12 +1,11 @@
-import { call, put, delay } from "redux-saga/effects";
+import { call, put } from "redux-saga/effects";
 import { fetchProjectsSuccess, fetchProjectsError } from "./portfolioSlice";
 import { getProjectsFromGithub } from ".././getProjectsFromGithub";
 
 function* fetchProjectsFromGithubHandler() {
     try {
-        yield delay(2000);
         const projects = yield call(getProjectsFromGithub);
-        yield put(fetchProjectsSuccess(projects));        
+        yield put(fetchProjectsSuccess(projects));
     } catch (error) {
         yield put(fetchProjectsError());
     }
--- src/getProjectsFromGithub.js
+++ src/getProjectsFromGithub.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export const getProjectsFromGithub = async () => {
-    //throw new Error("test");
-    const response = await axios.get("/personal-homepage/exampleProjects.json");
-    //const response = await axios.get("https://api.github.com/users/q270311/repos");
+    //const response = await axios.get("/personal-homepage/exampleProjects.json");
+    const response = await axios.get("https://api.github.com/users/q270311/repos");
     
     if (!response.ok) {
         new Error(response.statusText);
