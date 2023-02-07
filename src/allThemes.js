4f67ffb4cf09b013c32b245f4eb1f9c9a75344fe
Michał Bałdyga <q270311@gmail.com>
2023-02-07 22:20:00 +0100

    allThemes.js file created

---
+++ src/allThemes.js
@@ -0,0 +1,41 @@
+export const themes =
+{
+    "whiteMode": {
+        "color": {
+            "whiteText": "#ffffff",
+            "mainBackground": "#FBFBFE",
+            "blackText": "#252525",
+            "lightGrey": "#E5E5E5",
+            "mercury": "#E5E5E5",
+            "iron": "rgba(209, 213, 218, 0.3)",
+            "slateGray": "#6E7E91",
+            "mineShaft": "rgba(54, 54, 54, 0.72)",
+            "textSecondary": "rgba(110, 126, 145, 1)",
+            "mainBlue": "rgba(37, 23, 235, 1)",
+            "violet02": "rgba(9, 10, 51, 0.02)",
+            "violet03": "rgba(9, 10, 51, 0.03)"
+        },
+        "breakpoint": {
+            "mobileMax": 767
+        }
+    },
+    "darkMode": {
+        "color": {
+            "white": "#ffffff",
+            "blackText": "#252525",
+            "lightGrey": "#E5E5E5",
+            "mercury": "#E5E5E5",
+            "iron": "rgba(209, 213, 218, 0.3)",
+            "slateGray": "#6E7E91",
+            "mineShaft": "rgba(54, 54, 54, 0.72)",
+            "mainBackground": "rgba(37, 37, 37, 1)",
+            "textSecondary": "rgba(110, 126, 145, 1)",
+            "mainBlue": "rgba(37, 23, 235, 1)",
+            "violet02": "rgba(9, 10, 51, 0.02)",
+            "violet03": "rgba(9, 10, 51, 0.03)"
+        },
+        "breakpoint": {
+            "mobileMax": 767
+        }
+    }
+};
