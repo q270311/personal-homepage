d7fdaef6d4d69ee686d62e09a1154df204a068a1
Michał Bałdyga <q270311@gmail.com>
2023-02-08 07:53:27 +0100

    removed Body component

--- src/common/Body/styled.js
+++ src/common/Body/styled.js
@@ -2,6 +2,5 @@ import styled from "styled-components";
 
 export const StyledBody = styled.div`
     width: 100%;
-    background-color: ${({ theme }) => theme.color.mainBackground}; 
-    transition: background-color 800ms linear;
+    
 `;
