06704d815564b048577792ed316d9d62c603bd06
Michał Bałdyga <q270311@gmail.com>
2023-02-08 07:50:41 +0100

    defined colors in allThemes

--- src/allThemes.js
+++ src/allThemes.js
@@ -1,19 +1,12 @@
 export const themes =
 {
     "whiteMode": {
-        "color": {
-            "whiteText": "#ffffff",
-            "mainBackground": "#FBFBFE",
-            "blackText": "#252525",
-            "lightGrey": "#E5E5E5",
-            "mercury": "#E5E5E5",
-            "iron": "rgba(209, 213, 218, 0.3)",
-            "slateGray": "#6E7E91",
-            "mineShaft": "rgba(54, 54, 54, 0.72)",
-            "textSecondary": "rgba(110, 126, 145, 1)",
-            "mainBlue": "rgba(37, 23, 235, 1)",
-            "violet02": "rgba(9, 10, 51, 0.02)",
-            "violet03": "rgba(9, 10, 51, 0.03)"
+        "color": {            
+            "mainBackground": "#FBFBFE", 
+            "textHeader": "#ffffff", 
+            "textColor": "#6E7E91",
+            "backgroundToggleButton": "#E5E5E5",
+            "borderToggleButton" : "rgba(209, 213, 218, 0.3)"
         },
         "breakpoint": {
             "mobileMax": 767
@@ -21,18 +14,11 @@ export const themes =
     },
     "darkMode": {
         "color": {
-            "white": "#ffffff",
-            "blackText": "#252525",
-            "lightGrey": "#E5E5E5",
-            "mercury": "#E5E5E5",
-            "iron": "rgba(209, 213, 218, 0.3)",
-            "slateGray": "#6E7E91",
-            "mineShaft": "rgba(54, 54, 54, 0.72)",
             "mainBackground": "rgba(37, 37, 37, 1)",
-            "textSecondary": "rgba(110, 126, 145, 1)",
-            "mainBlue": "rgba(37, 23, 235, 1)",
-            "violet02": "rgba(9, 10, 51, 0.02)",
-            "violet03": "rgba(9, 10, 51, 0.03)"
+            "textHeader": "white",
+            "textColor": "white",
+            "backgroundToggleButton": "#6E7E91",
+            "borderToggleButton" : "white"
         },
         "breakpoint": {
             "mobileMax": 767
