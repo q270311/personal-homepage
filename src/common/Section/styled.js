49c298ccfb213d0691d1d793b0472f33a06fec18
Michał Bałdyga <q270311@gmail.com>
2023-02-09 13:01:16 +0100

    darkMode attribut from StyledSection removed

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,15 +1,11 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const StyledSection = styled.section`    
     padding: 36px;
     border-radius: 4px;
-    background-color: ${({ theme }) => theme.color.white};
+    background-color: ${({ theme }) => theme.color.whiteMineShaft};
     transition: background-color 800ms linear;
-    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);   
-
-    ${({ darkMode }) => darkMode && css`
-        background-color: ${({ theme }) => theme.color.mineShaft};
-    `};
+    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) { 
         margin: 14px;
@@ -23,14 +19,10 @@ export const Title = styled.div`
     line-height: 36px;
     text-align: left;
     letter-spacing: 0.05em;
-    color: ${({ theme }) => theme.color.blackText};
+    color: ${({ theme }) => theme.color.textHeader};
 
     padding-bottom: 16px;
     border-bottom: 1px solid ${({ theme }) => theme.color.lightGrey};
-
-    ${({ darkMode }) => darkMode && css`
-        color: ${({ theme }) => theme.color.white};
-    `};
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         padding-bottom: 12px;
