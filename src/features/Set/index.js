2957794b84aa18dba341e80f0cde61743fa2f430
Michał Bałdyga <q270311@gmail.com>
2023-02-09 13:02:31 +0100

    darkMode attribut removed from Set component

--- src/features/Set/index.js
+++ src/features/Set/index.js
@@ -1,25 +1,20 @@
-import { useSelector } from 'react-redux';
-import { selectThemeMode } from '../../appSlice';
 import Section from '../../common/Section';
 import { Ul, Li } from './styled';
 
-const Set = ({ skills, title }) => {
-    const darkMode = useSelector(selectThemeMode);
+const Set = ({ skills, title }) => (
+    <Section
+        title={title}
+        body={
+            <Ul>
+                {skills.map(skill => (
+                    <Li key={skill.id}>
+                        {skill.name}
+                    </Li>
+                ))}
+            </Ul>
+        }
+    />
+);
 
-    return (
-        <Section
-            title={title}
-            body={
-                <Ul>
-                    {skills.map(skill => (
-                        <Li key={skill.id} darkMode={darkMode}>
-                            {skill.name}
-                        </Li>
-                    ))}
-                </Ul>
-            }
-        />
-    );
-};
 
 export default Set;
