acc7dff6f19e7a001297d3156081ce9f30b82e0c
Michał Bałdyga <q270311@gmail.com>
2023-03-09 15:08:23 +0100

    rename folder to ContactSection

--- src/features/ContactSection/index.js
+++ src/features/ContactSection/index.js
@@ -5,8 +5,11 @@ import SVGFacebook from './SVGFacebook';
 import SVGGithub from './SVGGithub';
 import SVGLinkedIn from './SVGLinkedIn';
 
-const ContactsSocials = () => {
+const ContactSection = () => {
     const darkMode = useSelector(selectThemeMode);
+    const iconProps = {
+		color: darkMode ? "white" : "black"
+	}
 
     return (
         <Footer>
@@ -14,7 +17,7 @@ const ContactsSocials = () => {
             <Name>baldyga.software@gmail.com</Name>
             <ShortDescription>I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me</ShortDescription>
             <SocialsMedia>
-                <a href='https://github.com/q270311'><SVGGithub color={darkMode ? "white" : "black"} /></a>
+                <a href='https://github.com/q270311'><SVGGithub {...iconProps} /></a>
                 <a href='https://www.linkedin.com/in/micha%C5%82-ba%C5%82dyga-383526251'><SVGLinkedIn color={darkMode ? "white" : "black"} /></a>
                 <a href='https://www.facebook.com/profile.php?id=100005246371155'><SVGFacebook color={darkMode ? "white" : "black"} /></a>
             </SocialsMedia>
@@ -22,4 +25,4 @@ const ContactsSocials = () => {
     );
 };
 
-export default ContactsSocials;
+export default ContactSection;
