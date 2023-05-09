446d0bf16f81416627b3bd73b9aa766913897d53
Michał Bałdyga <q270311@gmail.com>
2023-05-09 22:43:34 +0200

    skills.ts file created

---
+++ src/skills.ts
@@ -0,0 +1,36 @@
+export const skills: string[] = [
+  'Semantic & accessible HTML',
+  'Responsive Web Design',
+  'Teamwork',
+  'Markdown',
+  'Immutability',
+  'CSS BEM convention',
+  'CSS Grid',
+  'CSS Flexbox',
+  'React Router',
+  'Redux-Saga',
+  'Redux (Toolkit)',
+  'React Hooks',
+  'Error handling',
+  'Working with API (fetch, axios)',
+  'TanStack Query v4',
+  'Promises, Async/Await',
+  'Express.js',
+  // eslint-disable-next-line
+  'JavaScript: ES6+',
+  'GitHub Pull Requests & Review',
+  'Trello',
+  'Scrum',
+  'NPM',
+  'React',
+  'Git',
+];
+
+export const skillsToLearnNext: string[] = [
+  'React Context',
+  'Node.js',
+  'Cypress',
+  'Unit testing',
+  'TypeScript',
+  'JS classes',
+];
